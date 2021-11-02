9171eef0b9b01cf1863c7fd4b7546f33c668f232
Savannah <savmaxwell@gmail.com>
2021-11-02 17:04:51 -0600

    fixed typeerror in posts.js

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -10,7 +10,7 @@ const Posts = (props) => {
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
-      {posts.map(posts => (<Post likePost={likePost} posts={posts} key={posts.id} />)
+      {posts.map(posts => (<Post likePost={likePost} post={posts} key={posts.id} />)
       )}
     </div>
   );
