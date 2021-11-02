7ec6a321b15135eaf8dde65c080f74f246672c14
Savannah <savmaxwell@gmail.com>
2021-11-02 17:58:01 -0600

    finished MVP

--- src/App.js
+++ src/App.js
@@ -32,8 +32,13 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+
+    // const newLikes = posts.map(post => {
+    //   return post.id === postId ? {...post, likes: post.likes +1} : post;
+    // })
+    // setPosts(newLikes)
     setPosts(posts.map(post => {
-      return post.id === postId ? { ...postId, likes: post.likes + 1} : post;
+      return post.id === postId ? { ...post, likes: post.likes + 1} : post;
     }))
     return postId
   };
